2f4fd4e33c4eccf0af146a94bbf6cb78aa90d996
Yauheni Papkou <eugene.schekutiev@gmail.com>
2022-01-19 02:24:14 +0000

    Add custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -12,3 +12,17 @@ def index(request):
       "blog/index.html", 
       {"posts": posts},
     )
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(
+      Post, 
+      slug=slug,
+    )
+    return render(
+      request, 
+      "blog/post-detail.html", 
+      {"post": post},
+    )
+
+
