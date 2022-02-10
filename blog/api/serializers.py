71dbc03dc6d1d0ddcb45ac305382836a7b675389
Yauheni Papkou <eugene.schekutiev@gmail.com>
2022-02-10 02:17:59 +0000

    Finish related fields

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
-from blog.models import Post
-
+from blog.models import Post, Tag
+from blango_auth.models import User
 
 class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        slug_field="value", 
+        many=True, 
+        queryset=Tag.objects.all(),
+    )
+
+    author = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(), 
+        view_name="api_user_detail", 
+        lookup_field="email",
+    )
+
     class Meta:
         model = Post
         fields = "__all__"
         readonly = ["modified_at", "created_at"]
+        
+# The Post API won’t work now, until we have added the api_user_detail view
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # We’re including just a subset of fields, 
+        # because there’s sensitive data like the password hash 
+        # which we don’t want to include.
+        fields = ["first_name", "last_name", "email"]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
-
-from blog.api.serializers import PostSerializer
+from blango_auth.models import User
 from blog.models import Post
+from blog.api.serializers import PostSerializer, UserSerializer
 from rest_framework.authentication import SessionAuthentication
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
 
 class PostList(generics.ListCreateAPIView):
     authentication_classes = [SessionAuthentication]
@@ -15,3 +16,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
