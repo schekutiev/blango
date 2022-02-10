077f4d6a8a6b86938ecc2fd61ef09d36cf16b6e5
Yauheni Papkou <eugene.schekutiev@gmail.com>
2022-02-10 01:27:52 +0000

    Update (Fix) View and settings permissions.

--- blango/settings.py
+++ blango/settings.py
@@ -85,7 +85,7 @@ class Dev(Configuration):
           "rest_framework.authentication.TokenAuthentication",
       ],
       "DEFAULT_PERMISSION_CLASSES": [
-          "rest_framework.permissions.IsAuthenticated",
+          "rest_framework.permissions.IsAuthenticatedOrReadOnly",
       ],
     }
 
--- blog/api/views.py
+++ blog/api/views.py
@@ -6,12 +6,12 @@ from rest_framework.authentication import SessionAuthentication
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     authentication_classes = [SessionAuthentication]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
