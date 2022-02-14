9858fd3802d0a3c0c019c9a04523b6a038acdd61
Yauheni Papkou <eugene.schekutiev@gmail.com>
2022-02-14 06:27:03 +0000

    Add viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,7 +4,18 @@ from drf_yasg.views import get_schema_view
 import os
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
-from blog.api.views import PostList, PostDetail, UserDetail
+from rest_framework.routers import DefaultRouter
+from blog.api.views import (
+    # PostList, 
+    # PostDetail, 
+    UserDetail, 
+    TagViewSet,
+    PostViewSet,
+)
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,8 +28,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    # path("posts/", PostList.as_view(), name="api_post_list"),
+    # path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -37,4 +48,5 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
