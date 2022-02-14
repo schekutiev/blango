f6a9364a904c0122b0a60e71996aedd1586bc7a3
Yauheni Papkou <eugene.schekutiev@gmail.com>
2022-02-14 05:25:52 +0000

    Add Swagger UI

--- blango/settings.py
+++ blango/settings.py
@@ -61,6 +61,7 @@ class Dev(Configuration):
         'debug_toolbar',
         'rest_framework',
         'rest_framework.authtoken',
+        'drf_yasg',
     ]
 
     MIDDLEWARE = [
@@ -106,6 +107,13 @@ class Dev(Configuration):
     ]
 
     WSGI_APPLICATION = 'blango.wsgi.application'
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
+    }
 
     # Database
     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
